c2649f201eb2d3e8da9fe494139ee749ce292ef4
Jaime Alonso <jaimea111@gmail.com>
2025-02-19 15:37:20 -0500

    Adding lambda and api gateway creation

--- python_cdk/python_cdk_stack.py
+++ python_cdk/python_cdk_stack.py
@@ -1,6 +1,8 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_lambda as lambda_,
+    aws_apigateway as apigw,
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -11,7 +13,20 @@ class PythonCdkStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        lambda_function = lambda_.Function(
+            self,
+            "ventas",
+            runtime=lambda_.Runtime.PYTHON_3_12,
+            handler="lambda_function.lambda_handler",
+            code=lambda_.Code.from_asset("lambda")
+        )
 
+        # Create an API Gateway and connect it to the Lambda function
+        api = apigw.LambdaRestApi(
+            self, "ventasAPI",
+            handler=lambda_function,
+            proxy=True  # Set `proxy=True` for automatic API mapping to Lambda
+        )
         # example resource
         # queue = sqs.Queue(
         #     self, "PythonCdkQueue",
