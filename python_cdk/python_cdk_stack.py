01e210879c2dff954a3709be2567d8418525d860
Jaime Alonso <jaimea111@gmail.com>
2025-02-20 00:15:14 -0500

    Removing eventbridge

--- python_cdk/python_cdk_stack.py
+++ python_cdk/python_cdk_stack.py
@@ -25,6 +25,52 @@ class PythonCdkStack(Stack):
             code=lambda_.Code.from_asset("lambda")
         )
 
+        # Create Cloudwatch alarms for the Lambda function
+        # 1. Error Rate Alarm: Triggers when any errors occur in your Lambda function
+        errors_alarm = cloudwatch.Alarm(
+            self,
+            "LambdaErrorsAlarm",
+            metric=lambda_function.metric_errors(),
+            threshold=1,
+            evaluation_periods=1,
+            alarm_description="Alarm if the Lambda function has any errors",
+            alarm_name="LambdaFunctionErrors",
+        )
+
+        # 2. Invocation Duration Alarm: Monitors execution time and alerts if it exceeds 5 seconds
+        duration_alarm = cloudwatch.Alarm(
+            self,
+            "LambdaDurationAlarm",
+            metric=lambda_function.metric_duration(),
+            threshold=5000,  # 5 seconds
+            evaluation_periods=1,
+            alarm_description="Alarm if the Lambda function duration exceeds 5 seconds",
+            alarm_name="LambdaFunctionDuration",
+        )
+
+        # 3. Throttles Alarm: Alerts when your function is being throttled
+        throttles_alarm = cloudwatch.Alarm(
+            self,
+            "LambdaThrottlesAlarm",
+            metric=lambda_function.metric_throttles(),
+            threshold=1,
+            evaluation_periods=1,
+            alarm_description="Alarm if the Lambda function is being throttled",
+            alarm_name="LambdaFunctionThrottles",
+        )
+
+        # 4. Invocations Alarm (for monitoring activity)
+        invocations_alarm = cloudwatch.Alarm(
+            self,
+            "LambdaInvocationsAlarm",
+            metric=lambda_function.metric_invocations(),
+            threshold=1,
+            evaluation_periods=5,
+            comparison_operator=cloudwatch.ComparisonOperator.LESS_THAN_THRESHOLD,
+            alarm_description="Alarm if the Lambda function has low/no invocations",
+            alarm_name="LambdaFunctionInvocations",
+        )
+
         # Create an API Gateway and connect it to the Lambda function
         api = apigw.LambdaRestApi(
             self, "ventasAPI",
@@ -37,71 +83,5 @@ class PythonCdkStack(Stack):
         #     visibility_timeout=Duration.seconds(300),
         # )
 
-        # Programar una ejecución periódica con EventBridge (cada 5 minutos)
-        rule = events.Rule(
-            self,
-            "LambdaHealthCheckRule",
-            schedule=events.Schedule.rate(duration=cloudwatch.Duration.minutes(2))
-        )
-        rule.add_target(targets.LambdaFunction(lambda_function))
 
-        #  MÉTRICAS PARA MONITOREO DE LA LAMBDA
 
-        # Métrica de invocaciones exitosas
-        success_metric = cloudwatch.Metric(
-            namespace="AWS/Lambda",
-            metric_name="Invocations",
-            dimensions_map={"FunctionName": lambda_function.function_name},
-            statistic="sum",
-            period=cloudwatch.Duration.minutes(2)
-        )
-
-        # Métrica de errores
-        error_metric = cloudwatch.Metric(
-            namespace="AWS/Lambda",
-            metric_name="Errors",
-            dimensions_map={"FunctionName": lambda_function.function_name},
-            statistic="sum",
-            period=cloudwatch.Duration.minutes(2)
-        )
-
-        # Métrica de duración de ejecución
-        duration_metric = cloudwatch.Metric(
-            namespace="AWS/Lambda",
-            metric_name="Duration",
-            dimensions_map={"FunctionName": lambda_function.function_name},
-            statistic="avg",
-            period=cloudwatch.Duration.minutes(2)
-        )
-
-        # ALARMAS EN CLOUDWATCH
-
-        # Alarma si la Lambda no se ejecuta en 5 minutos
-        no_execution_alarm = cloudwatch.Alarm(
-            self,
-            "LambdaNoExecutionAlarm",
-            metric=success_metric,
-            threshold=1,  # Debe haber al menos 1 ejecución
-            evaluation_periods=1,
-            comparison_operator=cloudwatch.ComparisonOperator.LESS_THAN_THRESHOLD
-        )
-
-        # Alarma si la Lambda empieza a fallar con errores
-        error_alarm = cloudwatch.Alarm(
-            self,
-            "LambdaErrorAlarm",
-            metric=error_metric,
-            threshold=1,  # Se activará si hay al menos 1 error
-            evaluation_periods=1,
-            comparison_operator=cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD
-        )
-
-        # Alarma si la ejecución de Lambda es muy lenta
-        duration_alarm = cloudwatch.Alarm(
-            self,
-            "LambdaSlowResponseAlarm",
-            metric=duration_metric,
-            threshold=3000,  # 3000 ms = 3 segundos
-            evaluation_periods=1,
-            comparison_operator=cloudwatch.ComparisonOperator.GREATER_THAN_THRESHOLD
-        )
